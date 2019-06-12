336f2872351919d2f2349a6204f728d538acfecc
Lukas Geyrhofer <lukas.geyrhofer@gmx.at>
2019-06-12 11:27:02 +0300

    implemented checks to not generate error msg

--- makeplots.sh
+++ makeplots.sh
@@ -6,16 +6,21 @@ do
 	gnuplot $plotfile
 done
 
-	for epsfile in `ls *.eps`
-	do
-		echo "eps" $epsfile
-		epstopdf $epsfile
-	done
+if [[ -f *.eps ]]
+then
+    for epsfile in `ls *.eps`
+    do
+            echo "eps" $epsfile
+            epstopdf $epsfile
+    done
+fi
 
-	for svgfile in `ls *.svg`
-	do
-		echo "svg" $svgfile
-		logfile=`echo $svgfile | sed 's/\.svg/\.log/g'`
-		make_latexfig.sh $svgfile $1 > $logfile
-	done
-
+if [[ -f *.svg ]]
+then
+    for svgfile in `ls *.svg`
+    do
+            echo "svg" $svgfile
+            logfile=`echo $svgfile | sed 's/\.svg/\.log/g'`
+            make_latexfig.sh $svgfile $1 > $logfile
+    done
+fi
